b7607faac8a39ff87612a7b482ae7be39d8e03ac
mykhailo-s <mykhailo-s@stellars.tech>
2024-12-05 17:15:41 +0200

    resolved task

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,27 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  let stateClone = {...state};
+  let actionHistory = [];
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties':
+        stateClone = {...stateClone,...action.extraData}
+        break;
+      case 'removeProperties':
+        action.keysToRemove.forEach(key => {
+          delete stateClone[key];
+        });
+        break;
+      case 'clear':
+        stateClone = {};
+
+        break;
+      default:
+    }
+    actionHistory.push({...stateClone});
+  }
+  return actionHistory;
 }
 
 module.exports = transformStateWithClones;
